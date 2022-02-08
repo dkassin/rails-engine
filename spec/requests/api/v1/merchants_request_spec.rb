be40bcf8be096194b1dd865306cc3c7e025e848f
David Kassin <david.j.kassin@gmail.com>
2022-02-08 14:24:08 -0700

    Test and features for Get one merchant endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  let!(:merchant) { create_list(:merchant, 3) }
+
 
   it "sends a list of merchants" do
-
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(merchant.count).to eq(3)
+    expect(merchants[:data].count).to eq(3)
 
     merchants[:data].each do |merchant|
-      expect(merchant[:attributes]).to have_key(:id)
-      expect(merchant[:attributes][:id]).to be_an(Integer)
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id].to_i).to be_an(Integer)
 
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id].to_i).to be_an(Integer)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+
   end
 end
