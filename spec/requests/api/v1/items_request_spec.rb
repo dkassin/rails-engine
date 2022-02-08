4be9b5bbbd83c72adc009b7a56f648a89b7d761e
David Kassin <david.j.kassin@gmail.com>
2022-02-08 15:07:46 -0700

    Test and features, get one item endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -20,4 +20,20 @@ describe "Items API" do
       expect(item[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'can get one item by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id].to_i).to be_an(Integer)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+  end
 end
