83a3bf62bb814928f885e9995cb0e8304764df16
David Kassin <david.j.kassin@gmail.com>
2022-02-08 15:44:18 -0700

    Test and Features, create and delete and item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,18 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params)), status: :created
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.destroy(params[:id]))
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,6 +18,15 @@ describe "Items API" do
 
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
   end
 
@@ -35,5 +44,44 @@ describe "Items API" do
     expect(item[:data][:attributes]).to have_key(:name)
     expect(item[:data][:attributes][:name]).to be_a(String)
 
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+
   end
+
+  it "can create a new item and destroy an item"  do
+    merchant = create(:merchant)
+    item_params = ({
+                    name: 'Cartman Figurine',
+                    description: 'A statue of the coon',
+                    unit_price: 314.12,
+                    merchant_id: merchant.id
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    binding.pry
+    expect(Item.all.count).to eq(1)
+    delete "/api/v1/items/#{created_item.id}"
+
+    expect(response).to be_successful
+    expect(Item.all.count).to eq(0)
+    expect{Item.find(created_item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+
 end
