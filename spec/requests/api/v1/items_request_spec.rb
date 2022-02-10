bd1c2a6fb51fa947055fe1ec1ebdf9a895896901
David Kassin <david.j.kassin@gmail.com>
2022-02-10 13:14:15 -0700

    All sad path and edge cases added for Fetch all items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,6 +30,66 @@ describe "Items API" do
     end
   end
 
+  it "sends a list of Items but does not include dependent data of the resource" do
+
+    create_list(:item, 5)
+
+    get '/api/v1/items'
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(items[:data].count).to eq(5)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:type)
+      expect(item[:type]).to_not eq("Merchant")
+    end
+  end
+
+
+  it "sends a list of Items even if only 1 item is found" do
+
+    create_list(:item, 1)
+
+    get '/api/v1/items'
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(items[:data].count).to eq(1)
+    expect(items[:data]).to be_an(Array)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id].to_i).to be_an(Integer)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+
+  it "fetch all items returns an array even if not items are found" do
+
+
+    get '/api/v1/items'
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(items[:data].count).to eq(0)
+    expect(items[:data]).to be_an(Array)
+  end
+
   it 'can get one item by its id' do
     id = create(:item).id
 
@@ -198,6 +258,34 @@ describe "Items API" do
     expect(item[:data][:message]).to be_an(String)
     expect(item[:data][:message]).to eq("Merchant not found")
 
+  end
+
+  it 'find a single item which matches a min search term' do
+    o_items = create_list(:item, 10)
+
+    get "/api/v1/items/find?min_price=50"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id].to_i).to be_an(Integer)
+
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data][:type]).to be_an(String)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
   end
 
   it 'find a single item which matches a min search term' do
